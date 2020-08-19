c59c3711422e72ec96faee94a0eb28c58caafc97
Fanchen Bao <jonathonbao@gmail.com>
2020-08-18 20:47:24 -0400

    Daily code challenge complete for 08-18-2020

--- 2020_08_challenge/08_18_2020.py
+++ 2020_08_challenge/08_18_2020.py
@@ -3,31 +3,72 @@ from typing import List
 from collections import Counter
 
 
-class Solution:
-    # def process(self, d1, val_per_pos):
-
-
-
+class Solution1:
     def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+        """This is BFS in disguise"""
+        # special case
+        if N == 1:
+            return list(range(10))
+        if K == 0:
+            return [int(str(i) * N) for i in range(1, 10)]
+        # regular case
+        ref = [[] for _ in range(10)]  # build a reference for all digits
+        for i in range(10):
+            if 0 <= i + K <= 9:
+                ref[i].append(i + K)
+            if 0 <= i - K <= 9:
+                ref[i].append(i - K)
         res = []
-        all_pos = [[set() for _ in range(N - 1)] for _ in range(10)]
-        for i in range(1, 10):
-            pre_set = {i}
-            for cur_set in all_pos[i]:
-                for val in pre_set:
-                    if 0 <= val + K <= 9:
-                        cur_set.add(val + K)
-                    if 0 <= val - K <= 9:
-                        cur_set.add(val - K)
-                if cur_set:
-                    pre_set = cur_set
-                else:
-                    break
-            # if all_pos[i][0]:
-            #     res += self.process(i, all_pos[i])
+        for j in range(1, 10):
+            temp = [j]
+            for _ in range(N - 1):  # construct the remaining digits using ref
+                new_temp = []
+                for t in temp:
+                    for dig in ref[t % 10]:
+                        new_temp.append(t * 10 + dig)
+                temp = new_temp
+            res += temp
         return res
 
 
-sol = Solution()
+class Solution2:
+    def dfs(self, pre_val, res, N, K):
+        if not N:
+            res.append(pre_val)
+            return
+        if pre_val != 0 and 0 <= pre_val % 10 + K <= 9:
+            self.dfs(pre_val * 10 + pre_val % 10 + K, res, N - 1, K)
+        if K != 0 and 0 <= pre_val % 10 - K <= 9:
+            self.dfs(pre_val * 10 + pre_val % 10 - K, res, N - 1, K)
 
-sol.numsSameConsecDiff(9, 1)
+    def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+        """Traditional DFS"""
+        res = []
+        for i in range(10):
+            temp = []
+            self.dfs(i, temp, N - 1, K)
+            res += temp
+        return res
+
+
+class Solution3:
+    def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+        """Traditional BFS"""
+        queue = list(range(10))
+        while N - 1:
+            new_queue = []
+            while queue:
+                pre_val = queue.pop()
+                if pre_val != 0 and 0 <= pre_val % 10 + K <= 9:
+                    new_queue.append(pre_val * 10 + pre_val % 10 + K)
+                if K != 0 and 0 <= pre_val % 10 - K <= 9:
+                    new_queue.append(pre_val * 10 + pre_val % 10 - K)
+            queue = new_queue
+            N -= 1
+        return queue
+
+
+
+sol = Solution3()
+
+print(sol.numsSameConsecDiff(3, 3))
