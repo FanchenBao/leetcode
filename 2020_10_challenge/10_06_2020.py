c2384b354029aa807205935ac05845424ede97df
Fanchen Bao <jonathonbao@gmail.com>
2020-10-06 13:27:13 -0400

    Complete daily challenge for 10-06-2020

---
+++ 2020_10_challenge/10_06_2020.py
@@ -0,0 +1,23 @@
+# from pudb import set_trace; set_trace()
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        """85% ranking. Easy question"""
+        if not root:
+            return TreeNode(val=val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+
