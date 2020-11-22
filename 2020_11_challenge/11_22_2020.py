4a4825e67b041688dccbf906694f7bfdb0909568
Fanchen Bao <jonathonbao@gmail.com>
2020-11-22 14:23:15 -0500

    Complete daily challenge for 11-22-2020

---
+++ 2020_11_challenge/11_22_2020.py
@@ -0,0 +1,55 @@
+# from pudb import set_trace; set_trace()
+from typing import List
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        """97% ranking. This is a very easy problem"""
+        table = [
+            ".-",
+            "-...",
+            "-.-.",
+            "-..",
+            ".",
+            "..-.",
+            "--.",
+            "....",
+            "..",
+            ".---",
+            "-.-",
+            ".-..",
+            "--",
+            "-.",
+            "---",
+            ".--.",
+            "--.-",
+            ".-.",
+            "...",
+            "-",
+            "..-",
+            "...-",
+            ".--",
+            "-..-",
+            "-.--",
+            "--.."
+        ]
+        return len(set(''.join(table[ord(w) - 97] for w in word) for word in words))
+
+
+
+# sol = Solution3()
+# tests = [
+#     # ([1, 2, 3, 1], 3, 0, True),
+#     # ([1, 0, 1, 1], 1, 2, True),
+#     ([1, 5, 9, 1, 5, 9], 2, 3, False),
+#     # ([1, 4, 9, 1, 4, 9], 1, 3, True),
+#     # ([-1, -1], 1, -1, False),
+#     # ([1, 3, 6, 2], 1, 2, True),
+# ]
+
+# for i, (nums, k, t, ans) in enumerate(tests):
+#     res = sol.containsNearbyAlmostDuplicate(nums, k, t)
+#     if res == ans:
+#         print(f'Test {i}: PASS')
+#     else:
+#         print(f'Test {i}; Fail. Ans: {ans}, Res: {res}')
