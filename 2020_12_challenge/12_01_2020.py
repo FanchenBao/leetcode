449e0f72c44534f1100cdc843a9adbbf6955af9a
Fanchen Bao <jonathonbao@gmail.com>
2020-12-01 16:31:31 -0500

    Complete daily challenge for 12-01-2020

---
+++ 2020_12_challenge/12_01_2020.py
@@ -0,0 +1,36 @@
+# from pudb import set_trace; set_trace()
+from typing import List
+from collections import defaultdict
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        """First day of December's challenge. Very simple question.
+        """
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+# sol = Solution3()
+# tests = [
+#     # ([1, 2, 3, 1], 3, 0, True),
+#     # ([1, 0, 1, 1], 1, 2, True),
+#     ([1, 5, 9, 1, 5, 9], 2, 3, False),
+#     # ([1, 4, 9, 1, 4, 9], 1, 3, True),
+#     # ([-1, -1], 1, -1, False),
+#     # ([1, 3, 6, 2], 1, 2, True),
+# ]
+
+# for i, (nums, k, t, ans) in enumerate(tests):
+#     res = sol.containsNearbyAlmostDuplicate(nums, k, t)
+#     if res == ans:
+#         print(f'Test {i}: PASS')
+#     else:
+#         print(f'Test {i}; Fail. Ans: {ans}, Res: {res}')
