860f4e1c72151ddfbc56ff7cb5c47fd5c7e0629e
Fanchen Bao <jonathonbao@gmail.com>
2022-12-16 18:17:37 -0800

    Complete leetcode daily challenge 2022-12-16

---
+++ 2022_12_challenge/12_16_2022.py
@@ -0,0 +1,46 @@
+# from pudb import set_trace; set_trace()
+from typing import List
+import math
+
+
+class MyQueue:
+
+    def __init__(self):
+        """LeetCode 232
+
+        pop and peek are not O(1), but amortized O(1)
+
+        55 ms, faster than 48.87%
+        """
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+
+    def empty(self) -> bool:
+        return len(self.s1) + len(self.s2) == 0
+
+
+# sol = Solution2()
+# tests = [
+#     ("hello", "holle"),
+#     ("leetcode", "leotcede"),
+# ]
+
+# for i, (s, ans) in enumerate(tests):
+#     res = sol.reverseVowels(s)
+#     if res == ans:
+#         print(f'Test {i}: PASS')
+#     else:
+#         print(f'Test {i}; Fail. Ans: {ans}, Res: {res}')
--- LeetCode_2499.py
+++ LeetCode_2499.py
@@ -1,40 +1,76 @@
 # from pudb import set_trace; set_trace()
 from typing import List
 import math
+from collections import defaultdict, deque
 
 
 class Solution:
     def minimumTotalCost(self, nums1: List[int], nums2: List[int]) -> int:
-        indices = {}
-        res = 0
+        indices = defaultdict(deque)
         N = len(nums1)
         for i, n in enumerate(nums1):
-            if n != nums2[i]:
-                continue
-            j = indices.get(n, 0)
-            while j < N and (nums2[j] == n or nums1[j] == n):
-                j += 1
-            if j == N:
-                return -1
-            if j > 0:
-                nums1[i], nums1[j] = nums1[j], nums1[i]
-                res += i + j
-                indices[n] = j
+            if n == nums2[i]:
+                indices[n].append(i)
+        # print(indices)
+        if not indices:
+            return 0
+        res = 0
+        keys = sorted(indices.keys(), key=lambda k: -len(indices[k]))
+        # make sure we have best chance of swapping with 0 index
+        if nums1[0] in indices and nums2[0] in indices and nums1[0] != nums2[0]:
+            while indices[nums1[0]] and indices[nums2[0]]:
+                ii = indices[nums1[0]].popleft()
+                jj = indices[nums2[0]].popleft()
+                res += ii + jj
+                nums1[ii], nums1[jj] = nums1[jj], nums1[ii]
+        if nums1[0] in indices:
+            j = 0
+            while indices[nums1[0]]:
+                while j < len(keys) and (not indices[keys[j]] or nums1[0] == keys[j]):
+                    j += 1
+                if j == len(keys):
+                    break
+                ii = indices[nums1[0]].popleft()
+                jj = indices[keys[j]].popleft()
+                res += ii + jj
+                nums1[ii], nums1[jj] = nums1[jj], nums1[ii]
+        if nums2[0] in indices:
+            j = 0
+            while indices[nums2[0]]:
+                while j < len(keys) and (not indices[keys[j]] or nums2[0] == keys[j]):
+                    j += 1
+                if j == len(keys):
+                    break
+                ii = indices[nums2[0]].popleft()
+                jj = indices[keys[j]].popleft()
+                res += ii + jj
+                nums1[ii], nums1[jj] = nums1[jj], nums1[ii]
 
-        queue = list(range(N))
-        while queue:
-            tmp = []
-            for i in queue:
-                if nums1[i] == nums2[i]:
-                    if nums1[i] == nums1[0] or nums1[i] == nums2[0]:
-                        tmp.append(i)
-                    else:
-                        nums1[i], nums1[0] = nums1[0], nums1[i]
-                        res += i
-            if queue == tmp:
-                return -1
-            queue = tmp
-        return res
+
+        # print(keys)
+        i, j = 0, len(keys) - 1
+        # pair the numbers that need to be swapped. This way each swap handles
+        # two numbers.
+        while True:
+            while i < j and not indices[keys[i]]:
+                i += 1
+            while i < j and not indices[keys[j]]:
+                j -= 1
+            if i == j:
+                break
+            ii = indices[keys[i]].popleft()
+            jj = indices[keys[j]].popleft()
+            res += ii + jj
+            nums1[ii], nums1[jj] = nums1[jj], nums1[ii]
+            
+        # handle the remaining unswapped numbers, which are all the same.
+        if indices[keys[i]]:
+            for t in range(len(nums1)):
+                if nums1[t] != keys[i] and nums2[t] != keys[i]:
+                    res += t + indices[keys[i]].popleft()
+                    if not indices[keys[i]]:
+                        break
+        return res if not indices[keys[i]] else -1
 
 
 sol = Solution()
@@ -43,6 +79,11 @@ tests = [
     ([2,2,2,1,3], [1,2,2,3,3], 10),
     ([1,2,2], [1,2,2], -1),
     ([2, 4, 3, 1, 1], [4, 4, 3, 1, 5], 6),
+    ([1,2,3], [3,1,2], 0),
+    ([1, 3, 2, 2, 2, 5, 2, 5, 1, 3], [1, 5, 5, 2, 2, 2, 2, 5, 1, 2], 28),
+    ([3, 4, 4, 3, 5, 1, 2, 2, 3, 4], [4, 4, 2, 3, 4, 2, 5, 2, 5, 1], 11),
+    ([3, 4, 1, 4, 1, 1, 2, 3, 2, 2], [4, 4, 5, 3, 4, 3, 1, 3, 2, 4], 16),
+    ([2,1,2,2,1,4,1,5], [2,1,2,2,1,4,1,5], 28),
 ]
 
 for i, (nums1, nums2, ans) in enumerate(tests):
