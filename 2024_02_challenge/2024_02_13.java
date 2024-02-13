b29f13035531fc2255d0d39d1e5a84ece2f2c707
Fanchen Bao <jonathonbao@gmail.com>
2024-02-13 01:36:12 -0800

    Complete leetcode daily challenge 2024-02-13

---
+++ 2024_02_challenge/2024_02_13.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.math.*;
+
+/**
+ * Definition for a binary tree node.
+ */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+
+class Solution {
+    private boolean isPalindrome(String word) {
+        int i = 0;
+        int j = word.length() - 1;
+        while (i < j) {
+            if (word.charAt(i++) != word.charAt(j--))
+                return false;
+        }
+        return true;
+    }
+
+    public String firstPalindrome(String[] words) {
+        /*
+        LeetCode 2108
+        
+        1 ms, faster than 100.00%
+        */
+        for (String w : words) {
+            if (isPalindrome(w))
+                return w;
+        }
+        return "";
+    }
+}
+
+
+class Main{
+    public static void main(String[] args) {
+        String s = "acbbaca";
+        String t = "aba";
+        Solution sol = new Solution();
+        System.out.println(sol.minWindow(s, t));
+    }
+}
--- LeetCode_2801.py
+++ LeetCode_2801.py
@@ -6,6 +6,22 @@ from functools import lru_cache
 
 class Solution:
     def countSteppingNumbers(self, low: str, high: str) -> int:
+        """
+        The basic idea is to use DP to find the number of stepping numbers
+        given the first digit and the total number of digits. For example,
+        dp(3, 5) = the number of stepping numbers starting with 3 and having
+        5 digits.
+        
+        Then we break down the problem into finding all the stepping numbers
+        with the same starting digit as low and same length, finding the stepping
+        numbers with the same starting digit as high and same length, and then
+        finding all the stepping numbers in between.
+        
+        The tricky part is when low and high have the same number of digits and/or
+        the same first digit. I used a separate function to handle that scenario.
+        
+        O(10 * 10 * len(high)) 134 ms, faster than 94.74%
+        """
         
         @lru_cache(maxsize=None)
         def dp(st: int, num_digits: int) -> int:
@@ -45,38 +61,67 @@ class Solution:
                             tmp.append(d + 1)
                     elif (is_low and d > cur) or (not is_low and d < cur):
                         count += dp(d, len(edge) - idx)
-                        print(is_low, d, dp(d, len(edge) - idx))
                 queue = tmp
                 idx += 1
             if idx == len(edge) and queue:
                 count += 1
             return count
+        
+        def count_edge_case_both():
+            """
+            This function counts the number of stepping numbers between low
+            and high when low and high have the same number of digits and the
+            same first digit
+            """
+            queue = [int(low[0])]
+            idx = 0
+            count = 0
+            while queue and idx < len(low):
+                tmp = []
+                for d in queue:
+                    cur_low = int(low[idx])
+                    cur_high = int(high[idx])
+                    if d == cur_low == cur_high:
+                        if d > 0:
+                            tmp.append(d - 1)
+                        if d < 9:
+                            tmp.append(d + 1)
+                    elif cur_low < d < cur_high:
+                        count += dp(d, len(low) - idx)
+                    elif cur_low == d and d < cur_high:
+                        count += count_edge_case(low[idx:], True)
+                    elif cur_low < d and d == cur_high:
+                        count += count_edge_case(high[idx:], False)
+                queue = tmp
+                idx += 1
+            if idx == len(low) and queue:
+                count += 1
+            return count
 
-        res = count_edge_case(low, True) + count_edge_case(high, False)
-        print(count_edge_case(low, True), count_edge_case(high, False))
         if len(low) == len(high):
+            if low[0] == high[0]:
+                return count_edge_case_both()
+            
+            res = count_edge_case(low, True) + count_edge_case(high, False)
             if int(low[0]) < 9:
                 for st in range(int(low[0]) + 1, int(high[0])):
                     res += dp(st, len(low))
-                    print('low', st, dp(st, len(low)))
         else:
+            res = count_edge_case(low, True) + count_edge_case(high, False)
             # count the rest of the stepping numbers with the same number of digits
             # as low
             if int(low[0]) < 9:
                 for st in range(int(low[0]) + 1, 10):
                     res += dp(st, len(low))
-                    print('low', st, dp(st, len(low)))
             # count the rest of the stepping numbers with the same number of digits
             # as high
             if int(high[0]) > 1:
                 for st in range(int(high[0]) - 1, 0, -1):
                     res += dp(st, len(high))
-                    print('high', st, dp(st, len(high)))
             # count the stepping numbers in between
             for num_digits in range(len(low) + 1, len(high)):
                 for st in range(1, 10):
                     res += dp(st, num_digits)
-                    print('between', st, num_digits, res)
         return res % 1000000007
 
 
