d649f38ab099721b05a9872532a9387c47bb3113
Fanchen Bao <jonathonbao@gmail.com>
2024-05-18 08:03:01 -0700

    Complete leetcode daily challenge 2024-05-16

---
+++ 2024_05_challenge/2024_05_17.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.math.*;
+
+/**
+ * Definition for a binary tree node.
+ */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        /*
+         * LeetCode 1325
+         *
+         * Basic tree traversal. If the current node is a leaf node with
+         * target being the value, return null.
+         *
+         * O(N), 0 ms, faster than 100.00%
+         */
+        if (root == null)        
+            return null;
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        if (root.left == null && root.right == null && root.val == target)
+            return null;
+        return root;
+    }
+}
+
+
+class Main{
+    public static void main(String[] args) {
+        String s = "acbbaca";
+        String t = "aba";
+        Solution sol = new Solution();
+        System.out.println(sol.minWindow(s, t));
+    }
+}
