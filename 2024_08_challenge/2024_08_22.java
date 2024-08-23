b4e27a18d93289c937987549f1c459179c55404d
Fanchen Bao <jonathonbao@gmail.com>
2024-08-22 19:25:33 -0700

    Complete leetcode daily challenge 2024-08-21

---
+++ 2024_08_challenge/2024_08_22.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.math.*;
+
+/**
+ * Definition for a binary tree node.
+ */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+
+class Solution {
+    public int findComplement(int num) {
+        /*
+         * LeetCode 476
+         *
+         * XOR
+         */
+        int cnt = 0;
+        int n = num;
+        while (n > 0) {
+            n >>= 1;
+            cnt++;
+        }
+        return ((1 << cnt) - 1) ^ num;
+    }
+}
+
+
+class Main{
+    public static void main(String[] args) {
+        String s = "acbbaca";
+        String t = "aba";
+        Solution sol = new Solution();
+        System.out.println(sol.minWindow(s, t));
+    }
+}
