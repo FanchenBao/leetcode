bc8ee61f2e1833982e623e09faeb12b205e2a50a
Fanchen Bao <jonathonbao@gmail.com>
2024-09-11 20:41:08 -0700

    Complete leetcode daily challenge 2024-09-10

---
+++ 2024_09_challenge/2024_09_11.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.math.*;
+
+/**
+ * Definition for a binary tree node.
+ */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        /*
+         * LeetCode 2220
+         *
+         * XOR and then bit count
+         * 0 ms, faster than 100.00%
+         */
+        return Integer.bitCount(start ^ goal);
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        String s = "acbbaca";
+        String t = "aba";
+        Solution sol = new Solution();
+        System.out.println(sol.minWindow(s, t));
+    }
+}
