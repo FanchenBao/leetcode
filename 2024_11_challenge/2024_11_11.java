b979f3ec7e1ba9bdf38429a130e01383770e01bc
Fanchen Bao <jonathonbao@gmail.com>
2024-11-11 22:05:47 -0800

    Complete leetcode daily challenge 2024-11-10

---
+++ 2024_11_challenge/2024_11_11.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.math.*;
+
+/**
+ * Definition for a binary tree node.
+ */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+
+class Solution {
+    public boolean primeSubOperation(int[] nums) {
+        /*
+         * LeetCode 2601
+         *
+         * For each n in nume, find the largest prime that is smaller than it.
+         * We can get the delta. For the next n, we find the largest prime
+         * that is smaller than n - delta. 
+         *
+         * The complication is when there is no prime that is smaller than
+         * n - delta. In this case, we can choose not to perform any operation
+         * if the current n is larger than delta.
+         *
+         * O(NlogN), 2 ms, faster than 100.00% 
+         */
+        int[] primes = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+        int delta = 0;
+        for (int n : nums) {
+            int idx = Arrays.binarySearch(primes, n - delta);
+            if (idx < 0)
+                idx = -(idx + 1);
+            if (idx == 0) {
+                if (n > delta) {
+                    delta = n; // do not perform the subtraction, use n directly
+                    continue;
+                }
+                return false;
+            }
+            delta = n - primes[idx - 1];
+        }
+        return true;
+    }
+}
+
+
+class Main{
+    public static void main(String[] args) {
+        String s = "acbbaca";
+        String t = "aba";
+        Solution sol = new Solution();
+        System.out.println(sol.minWindow(s, t));
+    }
+}
